71da593f9661bc5c10f27b06e9d9154ca42b0324
akshada-sapkal <akshadasapkal1@gmail.com>
2026-05-08 19:48:46 +0530

    Initial commit

---
+++ src/com/practice/Factorial.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+public class Factorial {
+	public static void main(String[] args) {
+		int num=5;
+		int fact=1;
+		while(num>0) {
+			fact=fact*num;
+			num--;
+		}
+		System.out.println("Factorial: "+fact);
+	}
+
+}
