46eeaa0a9e982f55d554a936a723cbb7ff5c0ca1
akshada-sapkal <akshadasapkal1@gmail.com>
2026-05-25 00:03:37 +0530

    Initial commit

---
+++ src/com/practice/ReverseString.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+public class ReverseString {
+	public static void main(String[] args) {
+		String s="Hello";
+		String s1="";
+		for(int i=s.length()-1;i>=0;i--) {
+			char c=s.charAt(i);
+			s1=s1+c;
+		}
+		System.out.println(s1);
+	}
+
+}
