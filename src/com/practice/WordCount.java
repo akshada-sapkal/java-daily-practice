bb5b8c48f1c39a813babdf4b927e03045f9d86ca
akshada-sapkal <akshadasapkal1@gmail.com>
2026-06-07 23:22:44 +0530

    Initial commit

---
+++ src/com/practice/WordCount.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+public class WordCount {
+	public static void main(String[] args) {
+		String s="Java is programming language";
+		String[] words=s.split(" ");
+		System.out.println(words.length);
+	}
+
+}
