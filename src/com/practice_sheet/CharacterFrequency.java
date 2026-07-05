b751dafbe0b17f6eb6f398b5a9d30db28f158ec2
akshada-sapkal <akshadasapkal1@gmail.com>
2026-07-05 23:31:06 +0530

    Initial commit

---
+++ src/com/practice_sheet/CharacterFrequency.java
@@ -0,0 +1,20 @@
+package com.practice_sheet;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CharacterFrequency {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the String: ");
+		String s=sc.next();
+		LinkedHashMap<Character, Integer> map=new LinkedHashMap<>();
+		for(char ch: s.toCharArray()) {
+			map.put(ch, map.getOrDefault(ch,0)+1);
+		}
+		for(Map.Entry<Character, Integer> entry:map.entrySet()) {
+			System.out.println(entry.getKey()+ " "+entry.getValue());
+		}
+	}
+}
